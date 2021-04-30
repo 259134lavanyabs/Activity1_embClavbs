a05064f78079e164bb8202c24bcfedfb99e8d9d8
259134lavanyabs <81295980+259134lavanyabs@users.noreply.github.com>
2021-04-30 13:23:06 +0530

    Update seatheatingApp.c

--- seatheatingApp.c
+++ seatheatingApp.c
@@ -1,1 +1,27 @@
+#include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
+#include "activity4.h"
 
+
+/**
+ * @brief Main function where the code execution starts
+ * 
+ * @return int Return 0 if the program completes successfully
+ * @ when pin is Low, the LED will turn OFF
+ * @ when pin is High, the LED will turn ON
+ */
+int main(void)
+{
+	/* Initialize Peripherals */
+	peripheral_init();
+    Init_ADC();
+	Init_PWM();
+	Init_USART();
+
+	for(;;)
+	{
+       tempbuttonSensor();
+	}
+	return 0;
+}
